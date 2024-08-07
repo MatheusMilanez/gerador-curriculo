9e25df275ca1e9bd5e934dd437fec2c6c283c2b6
Carlos <carlos.azarro@gmail.comm>
2024-08-07 13:34:15 -0300

    Add Prettier configuration and improve form functionality
    Included a Prettier config file for consistent code formatting. Enhanced the form's behavior by adding input placeholder text, disabling fields during asynchronous operations, and attaching the PDF generation function to the button click event. Updated project metadata in package.json and package-lock.json files.

--- index.js
+++ index.js
@@ -1,90 +1,117 @@
-// CEP
-
-async function buscaCep(cep) {
-  try {
-    const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-    const dados = await resposta.json();
-
-    if (dados.erro) {
-      alert("CEP NÃO ENCONTRADO");
-      return;
-    }
-
-    document.getElementById("rua").value = dados.logradouro;
-    document.getElementById("bairro").value = dados.bairro;
-    document.getElementById("cidade").value = dados.localidade;
-    document.getElementById("uf").value = dados.uf;
-  } catch (error) {
-    alert("Erro ao buscar o CEP!");
-  }
-}
-
-//Evento CEP
-
-document.getElementById("cep").addEventListener("blur", () => {
-  const cep = this.value.replace(/\D/g, "");
-  if (cep.length === 8) {
-    buscaCep(cep);
-  }
-});
-
-//--------------------------------------------
+const btnGeneratePDF = document.getElementById("generate-pdf");
 
 //-- Pegar Dados Formulário
 
 // Dados Pessoais
-var foto = document.getElementById("formFile");
-var nome = document.getElementById("nome");
-var cep = document.getElementById("cep");
-var rua = document.getElementById("rua");
-var bairro = document.getElementById("bairro");
-var cidade = document.getElementById("cidade");
-var uf = document.getElementById("uf");
-var telefone = document.getElementById("telefone");
-var email = document.getElementById("email");
+const foto = document.getElementById("formFile");
+const nome = document.getElementById("nome");
+
+const cep = document.getElementById("cep");
+const rua = document.getElementById("rua");
+const bairro = document.getElementById("bairro");
+const cidade = document.getElementById("cidade");
+const uf = document.getElementById("uf");
+const telefone = document.getElementById("telefone");
+const email = document.getElementById("email");
 
 //Formação academica
-var nomeEscola = document.getElementById("nomeEscola");
-var nivel = document.getElementById("nivel");
-var dataConclusao = document.getElementById("dataConclusao");
+const nomeEscola = document.getElementById("nomeEscola");
+const nivel = document.getElementById("nivel");
+const dataConclusao = document.getElementById("dataConclusao");
 
 //Formação Profissional
-var nomeEmpresa = document.getElementById("nomeEmpresa");
-var cargo = document.getElementById("cargo");
-var periodo = document.getElementById("periodo");
-var principalAtividade = document.getElementById("principalAtividade");
+const nomeEmpresa = document.getElementById("nomeEmpresa");
+const cargo = document.getElementById("cargo");
+const periodo = document.getElementById("periodo");
+const principalAtividade = document.getElementById("principalAtividade");
+
+//--------------------------------------------
+
+async function buscaCep(cep) {
+    // add a placehlold to input
+    rua.value = "Buscando...";
+    bairro.value = "Buscando...";
+    cidade.value = "Buscando...";
+    uf.value = "Buscando...";
+
+    // disable the field
+    rua.disabled = true;
+    bairro.disabled = true;
+    cidade.disabled = true;
+    uf.disabled = true;
+
+    try {
+        const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const dados = await resposta.json();
+
+        if (dados.erro) {
+            alert("CEP NÃO ENCONTRADO");
+            return;
+        }
+
+        // add the new values to the field
+        rua.value = dados.logradouro;
+        bairro.value = dados.bairro;
+        cidade.value = dados.localidade;
+        uf.value = dados.uf;
+    } catch (error) {
+        // if error enable input and reset values
+        rua.value = "";
+        bairro.value = "";
+        cidade.value = "";
+        uf.value = "";
+
+        rua.disabled = false;
+        bairro.disabled = false;
+        cidade.disabled = false;
+        uf.disabled = false;
+
+        alert("Erro ao buscar o CEP!");
+    }
+}
+
+//Evento CEP
+
+cep.addEventListener("blur", (e) => {
+    const cep = e.target.value.replace(/\D/g, "");
+    if (cep.length === 8) {
+        buscaCep(cep);
+    }
+});
 
 //--------------------------------------------
 
 //Download PDF
+btnGeneratePDF.onclick = generatePDF;
+
 async function generatePDF() {
-  const { jsPDF } = window.jspdf;
-  if (window.jspdf) {
-    try {
-      const resume = document.getElementById("curriculo");
-      const canvas = await html2canvas(resume);
-      const imgData = canvas.toDataURL("image/png");
+    const { jsPDF } = window.jspdf;
+    if (window.jspdf) {
+        try {
+            const resume = document.getElementById("curriculo");
+            const canvas = await html2canvas(resume);
+            const imgData = canvas.toDataURL("image/png");
 
-      const doc = new jsPDF({
-        orientation: "portrait",
-        unit: "mm",
-        format: "a4",
-      });
+            const doc = new jsPDF({
+                orientation: "portrait",
+                unit: "mm",
+                format: "a4",
+            });
 
-      const imgProps = doc.getImageProperties(imgData);
-      const pdfWidth = doc.internal.pageSize.getWidth();
-      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+            const imgProps = doc.getImageProperties(imgData);
+            const pdfWidth = doc.internal.pageSize.getWidth();
+            const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
 
-      doc.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+            doc.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
 
-      // Save the PDF
-      doc.save("resume.pdf");
-    } catch (erro) {
-      console.error("ERROR:", erro);
+            // Save the PDF
+            doc.save("resume.pdf");
+        } catch (erro) {
+            console.error("ERROR:", erro);
+        }
+    } else {
+        console.log("BIBLIOTECA JSPDF NÃO ACESSADA");
     }
-  } else {
-    console.log("BIBLIOTECA JSPDF NÃO ACESSADA");
-  }
 }
 
 //---------------------
