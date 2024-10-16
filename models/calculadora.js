dad658500c768b7866325ad7af3a97c605b236e3
MatheusMilanez <matheusmmlopes19@gmail.com>
2024-10-16 12:10:17 -0300

    add pi/v1/statusEndpoint

--- models/calculadora.js
+++
@@ -1,6 +0,0 @@
-function somar(numb1, numb2) {
-  return numb1 * numb2;
-}
-
-exports.somar = somar;
-//Padrão de exportação chamado CommomJS
---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,5 @@
+function status(request, response){
+    response.status(200).json({chave:"Valor"})
+}
+
+export default status;
