bde288eba454075ef33751ef12c1a7be7cbc8b94
Matheus Milanez Marques Lopes <99884832+MatheusMilanez@users.noreply.github.com>
2024-10-21 13:01:01 +0000

    add  and  files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response){
-    response.status(200).json({chave:"Valor"})
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result);
+  response.status(200).json({ chave: "Valor" });
 }
 
 export default status;
